49b7d0cdf30f1e4d120748b522057269827bc866
Cim611 <tanapoj.l@chonsolution.com>
2025-12-07 23:32:00 +0700

    Update:

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Home from "./src/home/page";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function DefaultPage() {
   return (
     <div className="bg-red-100 w-full min-h-screen">
       <div>
         <Home />
+        <SpeedInsights />
       </div>
     </div>
   );
